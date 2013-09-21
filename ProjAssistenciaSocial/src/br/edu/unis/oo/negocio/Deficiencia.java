e3c12c69a1092c98424803c346f9bd7df4236953
Cinita <newgar@gmail.com>
2013-09-21 13:38:55 -0300

    Signed-off-by: Cinita <newgar@gmail.com>

---
+++ ProjAssistenciaSocial/src/br/edu/unis/oo/negocio/Deficiencia.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.unis.oo.negocio;
+
+/**
+ *
+ * @author Cintia
+ */
+public class Deficiencia {
+    
+    private int id;
+    private String descricao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+}
