dfa72425bf240bce846dd67bbf95597be887515e
unknown <alunos@15168L.lab.unis.edu.br>
2013-09-21 13:25:49 -0300

    Signed-off-by: João

---
+++ ProjAssistenciaSocial/src/br/edu/unis/oo/negocio/Benefico.java
@@ -0,0 +1,33 @@
+
+package br.edu.unis.oo.negocio;
+
+public class Benefico {
+    public int id;
+    public String descricao;
+    public boolean status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+}
