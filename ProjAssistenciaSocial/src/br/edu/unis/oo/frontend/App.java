d8878b40b8e66eaaf33be5b29eb67be8b7301ce7
unknown <alunos@15050l.lab.unis.edu.br>
2013-10-19 11:52:12 -0300

    Signed-off-by: unknown <alunos@15050l.lab.unis.edu.br>

---
+++ ProjAssistenciaSocial/src/br/edu/unis/oo/frontend/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.unis.oo.frontend;
+
+/**
+ *
+ * @author alunos
+ */
+public class App {
+    
+    public static void main(String args[]){
+        frmPrincipal f = new frmPrincipal();
+        f.setExtendedState(f.MAXIMIZED_BOTH);
+        f.setVisible(true);
+    }
+    
+}
